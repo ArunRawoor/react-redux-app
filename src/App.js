1cb101f51a62780256e834999b3ac6a02b0a59a1
ArunRawoor <130204306+ArunRawoor@users.noreply.github.com>
2024-02-15 13:29:15 +0530

    hii

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { increment, decrement } from './features/counterSlice';
+
 import Product from './components/Product';
 import products from './data/products';
 import Cart from './components/Cart';
@@ -10,14 +9,11 @@ import Cart from './components/Cart';
 
 
 function App() {
-  const dispatch = useDispatch();
-  // const counter = useSelector(state => state.counter.value);
+
 
   return (
     <div>
-      {/* <h1>Counter: {counter}</h1>
-      <button onClick={() => dispatch(increment())}>Increment</button>
-      <button onClick={() => dispatch(decrement())}>Decrement</button> */}
+  
        <Cart />
       <h1>Products</h1>
       {products.map(product => (
